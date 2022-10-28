d4cffd423ebef95c0b6fca9dc9dcaa47d1a189b9
Art40000 <artemirii@gmail.com>
2022-10-28 22:26:32 +0300

    С1

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -1,6 +1,7 @@
 package koschei;
 
 import koschei.config.AppConfig;
+import koschei.models.*;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,8 +9,19 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
+        Death8 death = applicationContext.getBean(Death8.class);
+        Needle7 needle = applicationContext.getBean(Needle7.class, death);
+        Egg6 egg = applicationContext.getBean(Egg6.class, needle);
+        Duck5 duck = applicationContext.getBean(Duck5.class, egg);
+        Rabbit4 rabbit = applicationContext.getBean(Rabbit4.class, duck);
+        Wood3 wood = applicationContext.getBean(Wood3.class, rabbit );
+        Ocean1 ocean = applicationContext.getBean(Ocean1.class);
+
         KoscheiTheDeathless koscheiTheDeathless =
                 applicationContext.getBean(KoscheiTheDeathless.class);
+        koscheiTheDeathless.setOcean(ocean);
+
         System.out.println(koscheiTheDeathless.getRulesByDeth());
     }
 }
--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -1,14 +1,18 @@
 package koschei.models;
 
+import koschei.config.AppConfig;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Ocean1 {
 
-    @Autowired
     private Island2 island;
-
+    @Autowired
+    public Ocean1(Wood3 wood) {
+        island = AppConfig.getIsland(wood);
+    }
     @Override
     public String toString() {
         return "на океане остров " + island.toString();
